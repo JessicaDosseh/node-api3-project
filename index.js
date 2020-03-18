ab58d3e0745e7a5ec2fc8ce2a22f9974ca8b769b
Jessica <Jessica@users-iMac.local>
2020-03-17 21:21:10 -0400

    API 3 Project

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const app = require('./server.js'); 
+const port = 8000; 
+
+app.listen(port, () => {
+  console.log(`\n *** Server Listening on http://localhost:${port} ***`); 
+}); 
